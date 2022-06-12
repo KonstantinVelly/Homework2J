dfb4ac39ce6e3c4f2aaef36c6eb96314c77a0ba6
Konstantin Velly <konstantinvelly@gmail.com>
2022-06-12 14:38:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bonus = 1;
+        int cost = 1000;
+
+        int totalbonus = cost / 20 * bonus;
+
+        System.out.println("Общий бонус:");
+        System.out.println(totalbonus);
+
+    }
+}
